9592b673617e7555893e59b067daba2b14b51fe1
georgiosbr <georgios.botas@corusconsulting.com>
2019-11-19 13:23:37 +0100

    vistas

---
+++ main_layout/main_end.php
@@ -0,0 +1,20 @@
+      </div>
+      <!-- /.container-fluid -->
+
+      <!-- Sticky Footer -->
+      <?php include_once('layout/footer.html'); ?>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <?php include_once('layout/modal-logout-scroll-buttom.html'); ?>
+  
+  <!-- / Scripts and libs -->
+  <?php include_once('layout/scripts.html'); ?>
+  
+</body>
+
+</html>
---
+++ main_layout/main_ini.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin - Dashboard</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <?php include_once('layout/nav-bar.html'); ?>
+
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php include_once('layout/sidebar.html'); ?>
+
+    <div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <?php include_once('layout/breadcrumb.html'); ?>
